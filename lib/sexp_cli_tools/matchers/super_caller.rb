430fd2a8e9e9030eb4cd27a4811de672c02b17b7
Caleb Buxton <me@cpb.ca>
2021-11-15 22:49:17 -0800

    ✅ initially passing matching a call to super

--- lib/sexp_cli_tools/matchers/super_caller.rb
+++ lib/sexp_cli_tools/matchers/super_caller.rb
@@ -1,5 +1,6 @@
 module SexpCliTools
   module Matchers
-    SuperCaller = Sexp::Matcher.parse('[include (call nil super ___)]')
+    # Thanks to this test: https://github.com/seattlerb/sexp_processor/blob/93712e31b6d5e23c7d68cea805b40a642aad3e10/test/test_sexp.rb#L1625
+    SuperCaller = Sexp::Matcher.parse('[child (super ___)]')
   end
 end
