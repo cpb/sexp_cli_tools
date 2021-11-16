9c72698c07b7dfa90cfb5da0dece0629021de9a4
Caleb Buxton <me@cpb.ca>
2021-11-15 22:53:43 -0800

    ✅ coverage for super without args

--- lib/sexp_cli_tools/matchers/super_caller.rb
+++ lib/sexp_cli_tools/matchers/super_caller.rb
@@ -1,6 +1,7 @@
 module SexpCliTools
   module Matchers
     # Thanks to this test: https://github.com/seattlerb/sexp_processor/blob/93712e31b6d5e23c7d68cea805b40a642aad3e10/test/test_sexp.rb#L1625
-    SuperCaller = Sexp::Matcher.parse('[child (super ___)]')
+    # zsuper I noticed while simplifying the examples
+    SuperCaller = Sexp::Matcher.parse('[child (super ___)]') | Sexp::Matcher.parse('[child (zsuper)]')
   end
 end
--- test/sexp_cli_tools/matchers/super_caller_test.rb
+++ test/sexp_cli_tools/matchers/super_caller_test.rb
@@ -5,12 +5,17 @@ describe "SexpCliTools::Matchers::SuperCaller" do
 
   let(:without_super_caller) { parse_file('bicycle.rb') }
   let(:with_super_caller) { parse_file('road_bike.rb') }
+  let(:with_super_caller_no_args) { parse_file('mountain_bike.rb') }
 
   it "is not satisfied by a ruby file without a method calling super" do
     _(subject).wont_be :satisfy?, without_super_caller
   end
 
-  it "is satsifeid by a ruby file with a method calling super" do
+  it "is satisfied by a ruby file with a method calling super(args)" do
     _(subject).must_be :satisfy?, with_super_caller
+  end
+
+  it "is satisfy by a ruby file with a call to super" do
+    _(subject).must_be :satisfy?, with_super_caller_no_args
   end
 end
