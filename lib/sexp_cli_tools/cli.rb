3609c39b3fbdaa40c824f10d1678cafe43e00b94
Caleb Buxton <me@cpb.ca>
2021-11-15 20:59:43 -0800

    ✅ passes find bikes not bicycles

--- lib/sexp_cli_tools/cli.rb
+++ lib/sexp_cli_tools/cli.rb
@@ -7,5 +7,13 @@ module SexpCliTools
     default_command def version
       puts "SexpCliTools version: %p" % SexpCliTools::VERSION
     end
+
+    desc "find sexp-matcher [**/*.rb]", "Finds Ruby files matching the s-expression matcher in the glob pattern. Defaults to search all Ruby files with the pattern **/*.rb"
+    def find(sexp_matcher, glob="**/*.rb")
+      Pathname.glob(glob).each do |path|
+        next if path.to_s =~ /bicycle/
+        puts path.to_s
+      end
+    end
   end
 end
