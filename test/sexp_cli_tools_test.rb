e0c7f5a4d15eb68129e67fd069b484465a11da11
Caleb Buxton <me@cpb.ca>
2021-11-15 23:47:43 -0800

    ✅ dynamic s-expression matcher search

--- test/sexp_cli_tools_test.rb
+++ test/sexp_cli_tools_test.rb
@@ -6,4 +6,13 @@ describe SexpCliTools do
   it "has a version number" do
     refute_nil SexpCliTools::VERSION
   end
+
+  describe "MATCHERS['(class ___)']" do
+    subject { SexpCliTools::MATCHERS['(class ___)'] }
+    let(:a_class_sexp) { RubyParser.new.parse('class Foobar; end') }
+
+    it "matches a class" do
+      _(subject).must_be :satisfy?, a_class_sexp
+    end
+  end
 end
