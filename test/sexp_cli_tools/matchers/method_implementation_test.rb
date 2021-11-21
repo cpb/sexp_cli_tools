5691d58bc7fc6d08c3661177b16297462925584c
Caleb Buxton <me@cpb.ca>
2021-11-21 11:34:43 -0800

    🚨 fixes block length cop

--- test/sexp_cli_tools/matchers/method_implementation_test.rb
+++ test/sexp_cli_tools/matchers/method_implementation_test.rb
@@ -2,37 +2,43 @@
 
 require 'test_helper'
 
-describe 'SexpCliTools::Matchers::MethodImplementation' do
+module SexpExamples
+  def self.included(base)
+    base.let(:sexp_with_initialize) { parse_file('road_bike.rb') }
+    base.let(:sexp_without_initialize) do
+      RubyParser.new.parse(<<~EMPTY_CLASS_DEFINITION)
+        class Scooter
+        end
+      EMPTY_CLASS_DEFINITION
+    end
+  end
+end
+
+describe 'SexpCliTools::Matchers::MethodImplementation.satisfy?' do
+  subject { SexpCliTools::Matchers::MethodImplementation }
+
+  include SexpExamples
+
+  it 'is satisfied by a ruby file which implements initialize' do
+    _(subject.satisfy?(sexp_with_initialize, 'initialize')).must_equal true
+  end
+
+  it 'is not satisfied by a ruby file without an implementation of initialize' do
+    _(subject.satisfy?(sexp_without_initialize, 'initialize')).must_equal false
+  end
+end
+
+describe 'SexpCliTools::Matchers::MethodImplementation#satisfy?(sexp)' do
   subject { SexpCliTools::Matchers::MethodImplementation.new(target_method) }
+  let(:target_method) { :initialize }
 
-  let(:sexp_with_initialize) { parse_file('road_bike.rb') }
-  let(:sexp_without_initialize) do
-    RubyParser.new.parse(<<~EMPTY_CLASS_DEFINITION)
-      class Scooter
-      end
-    EMPTY_CLASS_DEFINITION
+  include SexpExamples
+
+  it 'is satisfied by a ruby file which implements initialize' do
+    _(subject).must_be :satisfy?, sexp_with_initialize
   end
 
-  describe '.satisfy?(sexp, target_method)' do
-    subject { SexpCliTools::Matchers::MethodImplementation }
-
-    it 'is satisfied by a ruby file which implements initialize' do
-      _(subject.satisfy?(sexp_with_initialize, 'initialize')).must_equal true
-    end
-
-    it 'is not satisfied by a ruby file without an implementation of initialize' do
-      _(subject.satisfy?(sexp_without_initialize, 'initialize')).must_equal false
-    end
-  end
-
-  describe 'with target_method :initialize' do
-    let(:target_method) { :initialize }
-    it 'is satisfied by a ruby file which implements initialize' do
-      _(subject).must_be :satisfy?, sexp_with_initialize
-    end
-
-    it 'is not satisfied by a ruby file without an implementation of initialize' do
-      _(subject).wont_be :satisfy?, sexp_without_initialize
-    end
+  it 'is not satisfied by a ruby file without an implementation of initialize' do
+    _(subject).wont_be :satisfy?, sexp_without_initialize
   end
 end
