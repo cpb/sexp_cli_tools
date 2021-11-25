a15b099636b4c7332d5b9fa897d7ff948fb3a52a
Caleb Buxton <me@cpb.ca>
2021-11-24 16:14:11 -0800

    🚨 disambiguate division from regular expressions

--- test/sexp_cli_tools/cli_test.rb
+++ test/sexp_cli_tools/cli_test.rb
@@ -5,19 +5,19 @@ require 'test_helper'
 describe 'sexp' do
   include CliTestHelpers
 
-  it { _(subject).must_match /SexpCliTools version: "\d+\.\d+\.\d+"/ }
+  it { _(subject).must_match(/SexpCliTools version: "\d+\.\d+\.\d+"/) }
 end
 
 describe 'sexp find child-class' do
   include CliTestHelpers
 
   it "doesn't match our parent class" do
-    _(subject).wont_match /bicycle.rb/
+    _(subject).wont_match(/bicycle.rb/)
   end
 
   it 'does match our child class' do
-    _(subject).must_match /mountain_bike.rb/
-    _(subject).must_match /road_bike.rb/
+    _(subject).must_match(/mountain_bike.rb/)
+    _(subject).must_match(/road_bike.rb/)
   end
 end
 
@@ -25,12 +25,12 @@ describe 'sexp find parent-class' do
   include CliTestHelpers
 
   it "doesn't match our child classes" do
-    _(subject).wont_match /mountain_bike.rb/
-    _(subject).wont_match /road_bike.rb/
+    _(subject).wont_match(/mountain_bike.rb/)
+    _(subject).wont_match(/road_bike.rb/)
   end
 
   it 'does match our parent class' do
-    _(subject).must_match /bicycle.rb/
+    _(subject).must_match(/bicycle.rb/)
   end
 end
 
@@ -38,12 +38,12 @@ describe 'sexp find super-caller' do
   include CliTestHelpers
 
   it "doesn't match our parent class" do
-    _(subject).wont_match /bicycle.rb/
+    _(subject).wont_match(/bicycle.rb/)
   end
 
   it 'does match our child class' do
-    _(subject).must_match /mountain_bike.rb/
-    _(subject).must_match /road_bike.rb/
+    _(subject).must_match(/mountain_bike.rb/)
+    _(subject).must_match(/road_bike.rb/)
   end
 end
 
@@ -51,9 +51,9 @@ describe "sexp find '(class ___)'" do
   include CliTestHelpers
 
   it 'lists all files with classes defined' do
-    _(subject).must_match /bicycle.rb/
-    _(subject).must_match /mountain_bike.rb/
-    _(subject).must_match /road_bike.rb/
+    _(subject).must_match(/bicycle.rb/)
+    _(subject).must_match(/mountain_bike.rb/)
+    _(subject).must_match(/road_bike.rb/)
   end
 end
 
@@ -61,11 +61,11 @@ describe 'sexp find method-implementation initialize' do
   include CliTestHelpers
 
   it 'lists the bicycle.rb and road_bike.rb files, which implements the initialize' do
-    _(subject).must_match /bicycle.rb/
-    _(subject).must_match /road_bike.rb/
+    _(subject).must_match(/bicycle.rb/)
+    _(subject).must_match(/road_bike.rb/)
   end
 
   it "doest not list mountain_bike, doesn't have initialize" do
-    _(subject).wont_match /mountain_bike.rb/
+    _(subject).wont_match(/mountain_bike.rb/)
   end
 end
