262d982d57ac51a70d0706e8b6acd22b9d093f9f
Caleb Buxton <me@cpb.ca>
2021-11-27 14:05:05 -0800

    🧪 changed fixtures to fail tests to force refactor

--- test/fixtures/coupling_between_superclasses_and_subclasses/mountain_bike.rb
+++ test/fixtures/coupling_between_superclasses_and_subclasses/mountain_bike.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock =  args[:rear_shock]
+    super(args)
+  end
+
   def spares
     super.merge({ rear_shock: rear_shock })
+  end
+
+  def default_tire_size
+    '2.1'
   end
 end
--- test/fixtures/coupling_between_superclasses_and_subclasses/road_bike.rb
+++ test/fixtures/coupling_between_superclasses_and_subclasses/road_bike.rb
@@ -8,6 +8,10 @@ class RoadBike < Bicycle
     super(args)
   end
 
+  def spares
+    super.merge({ tape_color: tape_color })
+  end
+
   def default_tire_size
     '21'
   end
